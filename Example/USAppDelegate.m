233498a50e31d9ded56d2e2b1f45685b8b4481d1
Ullrich Schäfer <ullrich@seidbereit.de>
2013-12-25 21:26:35 +0100

    Initial Commit

---
+++ Example/USAppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  USAppDelegate.m
+//  Festool
+//
+//  Created by Ullrich Schäfer on 25/12/13.
+//  Copyright (c) 2013 Ullrich Schäfer. All rights reserved.
+//
+
+#import "USAppDelegate.h"
+
+@implementation USAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+
+@end
