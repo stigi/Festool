8f80638203aaa2703d8f675d0a728c12ac22e53f
Ullrich Schäfer <ullrich@seidbereit.de>
2013-12-25 21:23:11 +0100

    Initial Commit

---
+++ Festool/USAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  USAppDelegate.h
+//  Festool
+//
+//  Created by Ullrich Schäfer on 25/12/13.
+//  Copyright (c) 2013 Ullrich Schäfer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface USAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Festool/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Festool
+//
+//  Created by Ullrich Schäfer on 25/12/13.
+//  Copyright (c) 2013 Ullrich Schäfer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "USAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([USAppDelegate class]));
+    }
+}
